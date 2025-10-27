5061321cd21ba171adb449c94178d0d38a085022
Ali4623 <232p1a3341@gmail.com>
2025-10-27 20:20:19 +0530

    Create 3.a) AIM: Write a React program to implement a counter button using React useState hook.js

---
+++ 3.a) AIM: Write a React program to implement a counter button using React useState hook.js
@@ -0,0 +1,12 @@
+3.a) AIM: Write a React program to implement a counter button using React useState hook.
+import React, { useState } from 'react';
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+}
+export default Counter;
